35e758259a16b129bb47153740f3b6ec7038f343
Aaron Tam <aarontam9529@gmail.com>
2024-03-06 22:51:31 +1000

    readme: updated readme

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Black Jack (Phase 1)
 
-Partially functionable
+Currently Partially functionable
+
+## How to play Phase 1
+
+1. Click add button to play that player
+2. After desired number of players, Click `Start Game` button to start round
+3. Starting from the leftmost position to right, each player will be promted 2 option, `Deal` or `Stand`. If the total points on the hand is 21 or greater, the round will automatically go to the next position.
+4. When all Player have finished their round, the Dealer will draw cards until its hand is 17 or more.
+5. Round have finished
+6. To start a new round, Press `New Game` and repeat step 1.
+
+> [!NOTE]
+> Phase 1 of this project is expected to have bugs and the layout of the game is poorly design. Most technical and aesthetic issues is aimed to fix in Phase 2 of this project.
 
 ## Currently working on Phase 2
 
--- src/components/Game.ts
+++ src/components/Game.ts
@@ -9,7 +9,14 @@ export class Game {
     private currentTurn: Player | Dealer | null;
     private playerIndex: number
     private started: boolean;
-    
+
+    /**
+     * Manage a round of a black-jack game
+     * 
+     * @param allPlayer - All player instances in this round 
+     * @param currentDealer - Dealer instance in this round 
+     * @param currentDeck - The Deck instance of cards used 
+     */
     constructor(allPlayer: Player[] , currentDealer: Dealer, currentDeck: Deck) {
         this.players = allPlayer;
         this.deck = currentDeck;
@@ -19,18 +26,38 @@ export class Game {
         this.started = false;
     }
 
+    /**
+     * Return all players in this round
+     * @returns all player instances
+     */
     getPlayers() {
         return this.players;
     }
 
+    /**
+     * Return Dealer in this round
+     * @returns dealer instance
+     */
     getDealer() {
         return this.dealer;
     }
 
+    /**
+     * Return Deck
+     * @returns Deck instance
+     */
     getDeck() {
         return this.deck;
     }
 
+    /**
+     * Return the current player or dealer's card points as a string
+     * If one or more Ace in hand, calculate the total points of hand
+     * as Ace is 1 and 11. If Ace as 11 and total points exceed 21 points,
+     * Ace will only equals to 1
+     * @param current instance of current player of dealer
+     * @returns total points as string (if Ace is present in hand {A,10} => "11/21")
+     */
     getTotalPoints(current: Player | Dealer) {
         if (current instanceof Player) {
 
@@ -57,10 +84,18 @@ export class Game {
         
     }
 
+    /**
+     * Return the instance that is in current turn
+     * @returns return instance of current turn 
+     */
     checkTurn(){
         return this.currentTurn;
     }
     
+    /**
+     * Change currentTurn to the next instance
+     * @param check - string to indicate deal or hold for player
+     */
     nextTurn(check: string) {
 
          if ((this.currentTurn !== null && this.currentTurn!.getHandPoints()[0] >= 21 && check === "check") || check === "stand") {
@@ -80,6 +115,9 @@ export class Game {
         }
     }
 
+    /**
+     * Set the round start to true
+     */
     setStarted() {
         this.started = true;
 
@@ -91,10 +129,17 @@ export class Game {
 
     }
 
+    /**
+     * Return whether the game started or not
+     * @returns true if yes, else false
+     */
     isStarted() {
         return this.started;
     }
 
+    /**
+     * Dealer will draw cards untill its hand reached 17 or more
+     */
     dealerPlay() {
         while (true) {
 
@@ -109,6 +154,9 @@ export class Game {
         }
     }
 
+    /**
+     * Check whether the player has won after dealer draw its cards
+     */
     win() {
         if (this.dealer.getHandPoints()[0] > 21) {
             this.players.forEach((player)=> {
